f68648ce66f519c96a5d03986bcab9fc71af15ce
Henrik Gabrielyan <henriquegabrielyan@gmail.com>
2024-01-26 12:03:49 +0200

    Replaced DriveDataset with SegmentationDataset

--- dataset.py
+++ dataset.py
@@ -1,73 +1,47 @@
-from torch.utils.data import Dataset
-from PIL import Image
 import numpy as np
-import torch
+from pathlib import Path
+from PIL import Image
+from torchvision import transforms
+from torch.utils.data import Dataset
 
-import torchvision.transforms as transforms
 
-
-class DRIVECustomDataset(Dataset):
-    def __init__(self, deeplake_dataset, transform=None, target_transform=None):
+class SegmentationDataset(Dataset):
+    def __init__(self, data_path, subset, transform=None, mask_transform=None):
         """
-        Custom dataset for the DRIVE dataset loaded via DeepLake.
+        Custom dataset for image segmentation.
         Args:
-            deeplake_dataset: A DeepLake dataset object.
-            transform: Optional transform to be applied on a sample.
+            data_path (Path or str): Path to the dataset directory.
+            subset (str): Sub-directory for the dataset ('train', 'val', or 'test').
+            transform (callable, optional): Optional transform to be applied on the image.
+            target_transform (callable, optional): Optional transform to be applied on the mask.
         """
-        self.deeplake_dataset = deeplake_dataset
-        # self.transform = transform
-
+        self.data_path = Path(data_path) / subset
+        self.images_dir = self.data_path / "images"
+        self.masks_dir = self.data_path / "masks"
+        self.images = sorted(list(self.images_dir.glob("*.png")))
+        self.masks = sorted(list(self.masks_dir.glob("*.png")))
         self.transform = transforms.Compose([transforms.ToTensor()])
-        self.img_size = 256, 256
-        # self.target_transform = target_transform
+        self.img_size = 256, 256  # original size is 584, 565
 
     def __len__(self):
-        return len(self.deeplake_dataset["rgb_images"])
+        return len(self.images)
 
     def __getitem__(self, idx):
-        # Load the images and masks
-        image = Image.fromarray(self.deeplake_dataset["rgb_images"][idx].numpy())
-        image = image.convert("RGB")
-        # if self.transform:
-        #     image = self.transform(image)
+        image_path = self.images[idx]
+        mask_path = self.masks[idx]
 
-        target = Image.fromarray(
-            self.deeplake_dataset["manual_masks/mask"][idx].numpy()[:, :, 0]
-        )
-        target = target.convert("L")
+        image = Image.open(image_path).convert("RGB")
+        mask = Image.open(mask_path).convert("L")
 
-        image_transform = image.resize(self.img_size)
-        mask_transform = target.resize(self.img_size)
-        img = np.asarray(image_transform)
-        mask = np.asarray(mask_transform)
-        mask = mask / 255.0
+        image = image.resize(self.img_size)
+        mask = mask.resize(self.img_size)
+        image = np.asarray(image)
+        mask = np.asarray(mask)
+        mask = mask / 255.0  # np.unique(mask.flatten())
+        # TODO: threshold?
+        mask = np.where(mask > 0.5, 1, 0)
 
-        img_tensor = self.transform(img)
-        mask_tensor = self.transform(mask)
+        image = self.transform(image)
+        mask = self.transform(mask)
 
-        y_other = torch.ones(
-            [1, mask_tensor.shape[1], mask_tensor.shape[2]]
-        ) - torch.sum(mask_tensor, dim=0)
-        mask_tensor = torch.cat((mask_tensor, y_other), dim=0)
-        # plt.imshow(mask_tensor[0], cmap="gray")
-
-        # TODO:
-        sample = dict(image=img_tensor, mask=mask, trimap=mask_tensor)
-        # convert to other format HWC -> CHW
-        sample["image"] = np.moveaxis(img, -1, 0)
-        sample["mask"] = np.expand_dims(mask, 0)
-        return sample
-
-        # if self.target_transform:
-        #     target = self.target_transform(target)
-        # target = np.array(target)
-        # target[target != 0] = 1
-        # target = torch.from_numpy(target).long()
-        # return image, target
-
-
-# TODO: convert mask
-
-    # a = image.to("cpu")[0].numpy()
-    # a=np.moveaxis(a, 0, -1)
-    # plt.imshow(a, cmap="gray")
+        return image, mask
